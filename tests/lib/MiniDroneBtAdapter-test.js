bf3037a95053a23e4c997f94e4237d5bcada4688
Gauthier Legrand <gotleg@gmail.com>
2017-02-27 00:30:42 +0100

    Added unit tests for new settings write methods

--- tests/lib/MiniDroneBtAdapter-test.js
+++ tests/lib/MiniDroneBtAdapter-test.js
@@ -122,6 +122,54 @@ describe('MiniDroneBtAdapter', () => {
         'did not match expected picture buffer'), true);
     });
 
+    it('should write the correct max altitude buffer', () => {
+        const adapter = new MiniDroneBtAdapter();
+        const buff = new Buffer([0x02, 1 & 0xFF, 0x02, 0x08, 0x00, 0x00, 2, 0x00 ]);
+        adapter.characteristics = mockCharacteristics;
+
+        adapter.writeMaxAltitude(2);
+        const spy = adapter.characteristics[1].write;
+        expect(spy).to.have.been.calledWith(sinon.match((value) =>
+            bufferEqual(value, buff),
+        'did not match expected max altitude buffer'), true);
+    });
+
+    it('should write the correct max tilt buffer', () => {
+        const adapter = new MiniDroneBtAdapter();
+        const buff = new Buffer([0x02, 1 & 0xFF, 0x02, 0x08, 0x01, 0x00, 10, 0x00 ]);
+        adapter.characteristics = mockCharacteristics;
+
+        adapter.writeMaxTilt(10);
+        const spy = adapter.characteristics[1].write;
+        expect(spy).to.have.been.calledWith(sinon.match((value) =>
+            bufferEqual(value, buff),
+        'did not match expected max tilt buffer'), true);
+    });
+
+    it('should write the correct max vertical speed buffer', () => {
+        const adapter = new MiniDroneBtAdapter();
+        const buff = new Buffer([0x02, 1 & 0xFF, 0x02, 0x01, 0x00, 0x00, 1, 0x00 ]);
+        adapter.characteristics = mockCharacteristics;
+
+        adapter.writeMaxVerticalSpeed(1);
+        const spy = adapter.characteristics[1].write;
+        expect(spy).to.have.been.calledWith(sinon.match((value) =>
+            bufferEqual(value, buff),
+        'did not match expected max vertical speed buffer'), true);
+    });
+
+    it('should write the correct max rotation speed buffer', () => {
+        const adapter = new MiniDroneBtAdapter();
+        const buff = new Buffer([0x02, 1 & 0xFF, 0x02, 0x01, 0x01, 0x00, 100, 0x00 ]);
+        adapter.characteristics = mockCharacteristics;
+
+        adapter.writeMaxRotationSpeed(100);
+        const spy = adapter.characteristics[1].write;
+        expect(spy).to.have.been.calledWith(sinon.match((value) =>
+            bufferEqual(value, buff),
+        'did not match expected max rotation speed buffer'), true);
+    });
+
     it('should correctly validate peripherals as drones', () => {
         const adapter = new MiniDroneBtAdapter();
         const isDrone = adapter.validatePeripheral(mockPeripheral);
